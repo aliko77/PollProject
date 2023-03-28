cfb8e6b3878209e93e76e2f1ed41cf95bb8a95c5
alikoc <akoc3273@gmail.com>
2023-03-28 14:57:56 +0300

    push

--- PollProject/settings.py
+++ PollProject/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_dev')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -130,7 +131,7 @@ TAILWIND_APP_NAME = "theme"
 
 # Tailwind npm bin path
 
-# NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 # Accounts Urls
 
